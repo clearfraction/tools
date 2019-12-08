0ba7088faf01f0fa8cab4912fe35fa26808a51f0
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2019-12-08 16:01:49 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # install rpm devtools
 cd /home
 swupd bundle-add package-utils curl
@@ -11,16 +12,19 @@ dnf -y groupinstall build srpm-build
 spectool -g *.spec
 dnf -y builddep *.spec
 
-
 # build the package
 rpmbuild -bb *.spec --define "_sourcedir $PWD"
-
 
 # deployment
 echo "start deployment"
 count=`ls -1 /rpmbuild/RPMS/*/*.rpm 2>/dev/null | wc -l`
 if [ $count != 0 ]
 then 
-echo true
+git clone -b repos --depth=1 https://gitlab.com/clearfraction/repository.git /tmp/repository
+mv /rpmbuild/RPMS/*/*.rpm /tmp/repository
+createrepo_c --database --compatibility /tmp/repository
+cd /tmp/repository && rm -rf .git
+git add .
+git -c user.name='CI' -c user.email='ci@ci.com' commit -m 'rebuild the repositories'
+git push -f https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos 
 fi 
-echo $count
