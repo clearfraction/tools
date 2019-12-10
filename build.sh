6e41333ed6d8a6828598c6947af188d397e45b35
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2019-12-11 01:16:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,11 +21,10 @@ echo "start deployment"
 count=`ls -1 /rpmbuild/RPMS/*/*.rpm 2>/dev/null | wc -l`
 if [ $count != 0 ]
 then 
-git clone -b repos --depth=1 https://gitlab.com/clearfraction/repository.git /tmp/repository
+git clone https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git /tmp/repository
 mv /rpmbuild/RPMS/*/*.rpm /tmp/repository
 createrepo_c --database --compatibility /tmp/repository
-cd /tmp/repository && rm -rf .git
-git init && git checkout -b repos && git add .
+cd /tmp/repository && git checkout -b repos && git add .
 git -c user.name='CI' -c user.email='ci@ci.com' commit -m 'rebuild the repositories'
 git push -f https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos
 fi 
