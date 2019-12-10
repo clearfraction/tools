cc8446c9a80f3437317508f0aa921d3c466f9e02
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2019-12-11 00:45:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ git clone -b repos --depth=1 https://gitlab.com/clearfraction/repository.git /tm
 mv /rpmbuild/RPMS/*/*.rpm /tmp/repository
 createrepo_c --database --compatibility /tmp/repository
 cd /tmp/repository && rm -rf .git
-git init && git add .
+git init && git checkout -b repos && git add .
 git -c user.name='CI' -c user.email='ci@ci.com' commit -m 'rebuild the repositories'
-git push -f https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos 
+git push -f https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos
 fi 
