2c1f0adc6689b1b8d1d68066673b24c152557ea3
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2019-12-16 15:07:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # install rpm devtools
 cd /home
-swupd update
-swupd bundle-add package-utils curl
+swupd update  1>/dev/null
+swupd bundle-add package-utils curl  1>/dev/null
 curl -L https://gist.github.com/paulcarroty/ec7133a6d41762e23cdacc75dab69423/raw/9869938ddb4471b177d27de8bffdea7fd4673099/spectool -o /usr/bin/spectool
 chmod +x /usr/bin/spectool
 
