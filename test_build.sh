e40659fae8085ea45f97cf9f961f4c1c9345e488
davidva <davidjeremias82@gmail.com>
2020-01-10 01:16:56 -0600

    absolute path changed in test

--- test_build.sh
+++ test_build.sh
@@ -26,11 +26,14 @@ exit
   done
 }
 
+
 #  BEGIN THE PROGRAM
 readargs "$@"
 
+# Absolute paths
+directory=/root/rpmbuild
+mkdir -p ${directory}/{RPMS,BUILD,SOURCES,SRPMS} && pushd ${directory}
 # install rpm devtools
-pushd /home
 swupd update  1>/dev/null
 swupd bundle-add package-utils curl 1>/dev/null
 curl -L https://gist.github.com/paulcarroty/ec7133a6d41762e23cdacc75dab69423/raw/9869938ddb4471b177d27de8bffdea7fd4673099/spectool -o /usr/bin/spectool
@@ -40,18 +43,16 @@ dnf config-manager --add-repo https://download.clearlinux.org/current/x86_64/os/
 dnf config-manager --add-repo https://gitlab.com/clearfraction/repository/raw/repos/
 dnf -q -y groupinstall build srpm-build
 # Cloning repository
-rm -rf ${namegit} && git clone https://github.com/clearfraction/${namegit}.git && pushd ${namegit}
+rm -rf ${namegit} && git clone https://github.com/clearfraction/${namegit}.git && pushd ${namegit}  
 # Downloading sources
 spectool -g *.spec
 # Installing build dependencies
 dnf -q -y builddep *.spec
 # build the package
 # rpmbuild --quiet  - super useful to cut the logs
-rpmbuild --define "_topdir $PWD" --define "_sourcedir $PWD" -bs *.spec
-rpmbuild --define "_topdir $PWD" --rebuild $PWD/SRPMS/*.src.rpm
-
+rpmbuild --define "_topdir $PWD" --define "_sourcedir $PWD" -bs *.spec && rpmbuild --define "_topdir $PWD" --rebuild $PWD/SRPMS/*.src.rpm
 # Test install
-pushd $PWD/RPMS
+pushd ${directory}/${namegit}/RPMS/x86_64/
 dnf -y install *.rpm
 popd
  popd
