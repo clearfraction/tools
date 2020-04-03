3b32f511f84d6412b8608a5d6852039716cc4002
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2020-04-03 08:07:43 +0300

    remove spectool

--- build.sh
+++ build.sh
@@ -12,10 +12,7 @@ clear_proxy
 
 # install rpm devtools
 cd /home
-swupd update  1>/dev/null
 swupd bundle-add package-utils curl  1>/dev/null
-curl -L https://gist.github.com/paulcarroty/ec7133a6d41762e23cdacc75dab69423/raw/9869938ddb4471b177d27de8bffdea7fd4673099/spectool -o /usr/bin/spectool
-chmod +x /usr/bin/spectool
 
 # manage dependencies
 dnf config-manager --add-repo https://download.clearlinux.org/current/x86_64/os/
