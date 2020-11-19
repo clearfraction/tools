359cc2d8c1069e426a510588d56387ddc5b3beec
Pavlo Rudyi <paulcarroty@users.noreply.github.com>
2020-11-19 22:35:27 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,5 +39,5 @@ createrepo_c --database --compatibility /tmp/repository
 cd /tmp/repository && rm -rf .git && git init && git checkout -b repos
 git add .
 git -c user.name='GitlabCI' -c user.email='gitlab@gitlab.com' commit -m 'rebuild the repositories'
-git push -f https://:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos
+git push -f https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos
 fi 
