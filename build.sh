8f4a042c183b0fba948866d741daf5a0f57c4433
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2021-09-06 21:04:44 +0300

    disable needs_restarting plugin

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ swupd update --quiet
 swupd bundle-add curl dnf --quiet 
 
 # manage dependencies
-shopt -s expand_aliases && alias dnf='dnf -q -y --releasever=latest --disableplugin=changelog'
+shopt -s expand_aliases && alias dnf='dnf -q -y --releasever=latest --disableplugin=changelog,needs_restarting'
 dnf config-manager \
     --add-repo https://cdn.download.clearlinux.org/current/x86_64/os \
     --add-repo https://gitlab.com/clearfraction/repository/-/raw/repos
