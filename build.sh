9f998b4aae85c8ad35bba3aba92665dca35461ff
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2021-10-11 12:57:16 +0300

    add artifacts handler

--- build.sh
+++ build.sh
@@ -8,14 +8,15 @@ unset https_proxy
 # install rpm devtools
 cd /home
 swupd update --quiet 
-swupd bundle-add curl dnf --quiet 
+swupd bundle-add curl dnf mixer --quiet 
 
 # manage dependencies
 shopt -s expand_aliases && alias dnf='dnf -q -y --releasever=latest --disableplugin=changelog,needs_restarting'
+createrepo_c /home/artifact/
 dnf config-manager \
     --add-repo https://cdn.download.clearlinux.org/current/x86_64/os \
-    --add-repo https://gitlab.com/clearfraction/repository/-/raw/repos
-dnf groupinstall build srpm-build && dnf install createrepo_c
+    --add-repo file:///home/artifact
+dnf groupinstall build srpm-build
 dnf builddep *.spec
 
 # building the package
