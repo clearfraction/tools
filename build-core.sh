ebc371da4c9e7988c436d4e5791436ef8da4533a
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2021-10-11 13:09:36 +0300

    Update build-core.sh

--- build-core.sh
+++ build-core.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# identical to build.sh without Clear Fraction repositories
 
 # disable proxy
 unset http_proxy
@@ -12,22 +11,16 @@ swupd update --quiet
 swupd bundle-add curl dnf --quiet 
 
 # manage dependencies
-shopt -s expand_aliases && alias dnf='dnf -q -y --releasever=latest --disableplugin=changelog'
-dnf config-manager --add-repo https://cdn.download.clearlinux.org/current/x86_64/os
-dnf groupinstall build srpm-build && dnf install createrepo_c
+shopt -s expand_aliases && alias dnf='dnf -q -y --releasever=latest --disableplugin=changelog,needs_restarting'
+createrepo_c /home/artifact/
+dnf config-manager \
+    --add-repo https://cdn.download.clearlinux.org/current/x86_64/os
+dnf groupinstall build srpm-build
 dnf builddep *.spec
 
-# build the package
-# rpmbuild --quiet  - super useful to cut the logs
-# spectool fails some times (needs a hand) `--undefine=_disable_source_fetch`
+# building the package
 rpmbuild --quiet -bb *.spec --define "_topdir $PWD" \
          --define "_sourcedir $PWD" --undefine=_disable_source_fetch \
          --define "abi_package %{nil}"
-
-# deployment
-count=`ls -1 $PWD/RPMS/*/*.rpm 2>/dev/null | wc -l`
-if [ $count != 0 ]
-then
-echo "Start deployment..."
-mv $PWD/RPMS/*/*.rpm /tmp/repository
-fi 
+# post cleanup
+mv RPMS/*/*.rpm RPMS/
